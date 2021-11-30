c55006f6cfb18bb2ed384a1569aba49ca2b24379
Egor Astakhov <noise718@gmail.com>
2021-11-30 23:42:06 +0300

    Add ActionSheet with Image Picker

--- MyPlacesApp/NewPlaceViewController.swift
+++ MyPlacesApp/NewPlaceViewController.swift
@@ -20,6 +20,21 @@ class NewPlaceViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Если мы нажимаем на первую ячейку, то открывается меню выбора фотографии-картинки, а если мы нажимаем на другие ячейки, то прячется клавиатура
         if indexPath.row == 0 {
+            // Делаем алерт контроллер
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            self.present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
@@ -34,5 +49,22 @@ extension  NewPlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: - Work with image
+extension NewPlaceViewController {
+    // метод работы с изображениями
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            // позволяет пользователю редактировать изображение
+            imagePicker.allowsEditing = true
+            // определяем тип источника для выбранного изображения
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+        
     }
 }

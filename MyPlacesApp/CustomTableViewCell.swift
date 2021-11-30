f6a8715394cccad7ba454253d019d238de1c025e
Egor Astakhov <noise718@gmail.com>
2021-11-30 20:45:25 +0300

    Custom Table View Cell

---
+++ MyPlacesApp/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlacesApp
+//
+//  Created by Егор Астахов on 30.11.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}

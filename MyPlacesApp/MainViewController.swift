86a83559d3e5c6f11d793e0c1433d1a75b65ba60
Egor Astakhov <noise718@gmail.com>
2021-11-30 20:29:17 +0300

    Add icon, Launch Screen, Row Hight

--- MyPlacesApp/MainViewController.swift
+++ MyPlacesApp/MainViewController.swift
@@ -31,8 +31,19 @@ class MainViewController: UITableViewController {
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        // Скругляем image view
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        // Скругляем само изображение
+        cell.imageView?.clipsToBounds = true
 
         return cell
+    }
+    
+    // MARK: - Table View Delegate
+    
+    // Изменяем высоту строки
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     /*
